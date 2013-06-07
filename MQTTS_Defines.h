3c93a3ca8a580efe44a4d94f34b8509141a70139
Tomoaki YAMAGUCHI <tomoaki@tomy-tech.com>
2013-06-07 13:20:10 +0900

    Update MQTTS_Defines.h

--- MQTTS_Defines.h
+++ MQTTS_Defines.h
@@ -1,20 +1,28 @@
 #ifndef  MQTTS_DEFINES_H_
 #define  MQTTS_DEFINES_H_
 
-/*=========================
+/*=================================
+ * Device Type Default ARDUINO
+ ==================================*/
+#define LINUX
+//#define MBED
+
+//#define XBEE_FLOWCTL_CRTSCTS
+
+/*=================================
  *    Debug Condition
- ==========================*/
+ ==================================*/
 #define DEBUG_ZBEESTACK
-#define ZBEE_EMULATION
+#define DEBUG_MQTTS
 
 
-/*=========================
+/*=================================
  *    Data Type
- ==========================*/
+ ==================================*/
 #ifndef ARDUINO
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned long uint32_t;
+typedef unsigned long  uint32_t;
 #endif
 
 
