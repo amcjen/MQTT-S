b94e61726a4786ea3a5385d5b3d5ab996faae1d5
Tomoaki YAMAGUCHI <tomoaki@tomy-tech.com>
2013-06-11 14:25:10 +0900

    Update MqttsClientApp.cpp

--- MqttsClientApp.cpp
+++ MqttsClientApp.cpp
@@ -15,20 +15,34 @@
 #include <iostream>
 
 
+MQString* willtopic = new MQString("willtopic");
+MQString* willmsg   = new MQString("willmsg");
+
+
+int fnTp1(MqttsPublish* msg){
+  printf("Execute fnTp1\n");
+  return 0;
+}
+
+
+
 
 int main(int argc, char **argv){
 
   /* Client Test  */
 
 
-    MqttsClient mqtts = MqttsClient();
+
+  MqttsClient mqtts = MqttsClient();
 
 
 
     mqtts.begin(argv[1], B9600);
     mqtts.init("Node-02");
     mqtts.setQos(1);
-    mqtts.setKeepAlive(10000);
+    //mqtts.setWillTopic(willtopic);
+    //mqtts.setWillMessage(willmsg);
+    mqtts.setKeepAlive(60000);
 
 
     fprintf(stdout,"Connect\n");
@@ -36,17 +50,96 @@ int main(int argc, char **argv){
 
     while(true){
         int rc = mqtts.execMsgRequest();
-        if (rc){
-            fprintf(stdout,"Rc = %d, Request type = 0x%x\n", rc, mqtts.getMsgRequestType());
+        if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+            break;
+        }else{
+            fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
             if ( mqtts.getMsgRequestType() == MQTTS_TYPE_SEARCHGW){
-                sleep(5);
                 mqtts.setMsgRequestStatus(MQTTS_MSG_REQUEST);
             }else{
                 mqtts.clearMsgRequest();
             }
         }
-}
+    }
+/*
+    MQString *topic = new MQString("a/bcd/ef");
 
+    mqtts.registerTopic(topic);
+
+    while(true){
+            int rc = mqtts.execMsgRequest();
+        if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+            break;
+        }else{
+            fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
+                mqtts.clearMsgRequest();
+        }
+    }
+*/
+    /*
+    mqtts.disconnect();
+    while(true){
+        int rc = mqtts.execMsgRequest();
+        if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+            break;
+        }else{
+            fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
+                mqtts.clearMsgRequest();
+        }
+    }
+    */
+
+    /*
+    mqtts.willTopic();
+
+        while(true){
+            int rc = mqtts.execMsgRequest();
+            if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+                break;
+            }else{
+                fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
+                    mqtts.clearMsgRequest();
+            }
+        }
+     */
+
+    MQString *topic = new MQString("a/bcd/ef");
+
+    mqtts.subscribe(topic, MQTTS_TOPIC_TYPE_NORMAL, fnTp1);
+
+        while(true){
+            int rc = mqtts.execMsgRequest();
+            if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+                break;
+            }else{
+                fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
+                    mqtts.clearMsgRequest();
+            }
+        }
+
+/*
+
+        mqtts.unsubscribe(topic);
+
+            while(true){
+                int rc = mqtts.execMsgRequest();
+                if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+                    break;
+                }else{
+                    fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
+                        mqtts.clearMsgRequest();
+                }
+            }
+*/
+        while(true){
+                        int rc = mqtts.execMsgRequest();
+                        if (rc == MQTTS_ERR_NO_ERROR && mqtts.getMsgRequestCount() == 0){
+                            continue;
+                        }else{
+                            fprintf(stdout,"Rc = %d, Request status = 0x%x\n", rc, mqtts.getMsgRequestStatus());
+                                mqtts.clearMsgRequest();
+                        }
+                    }
 
      fprintf(stdout,"__end__\n");
 
